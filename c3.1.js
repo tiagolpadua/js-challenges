e8c2f5d02d48fb448107b359511769527cec2fed
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2023-11-22 23:27:15 -0300

    commit inicial

---
+++ c3.1.js
@@ -0,0 +1,13 @@
+// Escreva duas funções binárias: 'add' e 'mul', que recebem dois números e retornam a soma e o produto:
+
+function add(x, y) {
+    return x + y;
+}
+
+function mul(x, y) {
+    return x * y;
+}
+
+console.log('add(1,2)', add(1,2));
+
+console.log('mul(2,3)', mul(2,3));
